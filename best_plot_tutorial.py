872e4bc128215c64662273b76556b995f142729d
Stantecks <zhouy2@miamioh.edu>
2017-06-21 16:09:39 -0700

    attempted to complete lesson23-24 practice

--- best_plot_tutorial.py
+++ best_plot_tutorial.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy
 
 
 def ecdf(data):
@@ -17,11 +18,21 @@ sns.set(style='whitegrid', palette=colors, rc={'axes.labelsize': 16})
 data_hi = np.loadtxt('data/xa_high_food.csv', comments='#')
 data_lo = np.loadtxt('data/xa_low_food.csv', comments='#')
 
+# compute the CDF
+# Make smooth x-values
+x = np.linspace(1600, 2500, 400)
+
+# Compute theoretical Normal distribution
+cdf_theor = scipy.stats.norm.cdf(x, loc=np.mean(data_lo),
+                                 scale=np.std(data_lo))
+
 fig, ax = plt.subplots(1, 1)
 _ = ax.set_xlabel('X')
 _ = ax.set_ylabel('ECDF')
 _ = ax.plot(*ecdf(data_hi), marker='.', linestyle='none')
 _ = ax.plot(*ecdf(data_lo), marker='.', linestyle='none')
 
+# Don't know how to do a CDF nor the theory behind it yet
+# _ = ax.plot(x, cdf_theor)
 
 plt.show()
